29738ef673bbf58d0ebef71e75c298babbd95875
Robert Babaev <github@robertbabaev.tech>
2025-10-26 17:53:38 -0400

    fix: add smarter horizontal movement

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ if vim.g.vscode then
   -- VSCode neovim
   require "user.vscode_keymaps"
   vim.opt.clipboard = "unnamedplus"
+  vim.opt.whichwrap:append "<>[]hl"
 else
   -- Ordinary neovim
   vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
