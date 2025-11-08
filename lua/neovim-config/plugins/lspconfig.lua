7b846deea09889d2857398b4cf3bf53ee2a8dd84
Robert Babaev <github@robertbabaev.tech>
2025-11-07 19:16:53 -0500

    feat: add schemas for yamlls

--- lua/neovim-config/plugins/lspconfig.lua
+++ lua/neovim-config/plugins/lspconfig.lua
@@ -36,6 +36,23 @@ function ConfigureLsps()
     vim.lsp.enable(lsp)
   end
 
+  vim.lsp.config("yamlls", {
+    settings = {
+      yaml = {
+        schemas = {
+          kubernetes = "k8s-*.yaml",
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+          ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+          ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+        },
+      },
+    },
+  })
+
   vim.lsp.config("tinymist", {
     root_dir = function(filename, bufnr)
       return vim.fn.getcwd()
