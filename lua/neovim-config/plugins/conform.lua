f33ddfca6412193f489dc556736c206b65b90ff7
Robert Babaev <github@robertbabaev.tech>
2025-11-30 17:11:31 -0500

    style: switch nix formatter to nixfmt
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- lua/neovim-config/plugins/conform.lua
+++ lua/neovim-config/plugins/conform.lua
@@ -35,7 +35,7 @@ return {
       jsonc = Prettier_or_biome,
       vue = { "prettier" },
       xml = { "xmlformatter" },
-      nix = { "alejandra" },
+      nix = { "nixfmt" },
     },
     linters_by_ft = {
       make = { "checkmake" },
